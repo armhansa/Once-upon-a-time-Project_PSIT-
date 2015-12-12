25a49f7dab04ea006b7ea9092ce8d59d0f69ea32
winrhcp <winrhcp@outlook.com>
2015-12-12 21:09:31 +0700

    add values

--- Project.v2.py
+++ Project.v2.py
@@ -2,11 +2,11 @@ import csv
 import plotly.plotly as py
 from plotly.tools import FigureFactory as FF
 from datetime import datetime
-
+py.sign_in('winrhcp', 'vh4aobsxjl')
 yy, mm, dd = 2015, 12, 1
 name_hoon = input('ใส่ชื่อหุ้นที่ต้องการดูกราฟ : ').upper()
 day_in = int(input('จำนวนวันที่ต้องการดูย้อนหลังของหุ้นตัวนั้น : '))
-open_data, data_close, data_high, data_low, data_vol, date = [], [], [], [], [], []
+data_open, data_close, data_high, data_low, data_vol, data_values, date = [], [], [], [], [], [], []
 while day_in > 0:
     step_day = 'set-history_EOD_'+'%d-%02d-%02d.csv' % (yy, mm, dd)
     try:
@@ -19,6 +19,7 @@ while day_in > 0:
                 data_low.append(float(row[4]))
                 data_close.append(float(row[5]))
                 data_vol.append(float(row[6]))
+                data_values.append(float(row[6])*float(row[5]))
                 date.append(datetime(year = int(row[1][0:4]), month = int(row[1][4:6]), day = int(row[1][6:])))
 
         day_in -= 1
@@ -33,6 +34,6 @@ while day_in > 0:
         yy -= 1
         mm = 12
 
-fig = FF.create_candlestick(open_data, high_data, low_data, close_data, dates=dates)
+fig = FF.create_candlestick(data_open, data_high, data_low, data_close, dates=date)
 
 plot_url = py.plot(fig, filename='finance/simple-candlestick', validate=False)
