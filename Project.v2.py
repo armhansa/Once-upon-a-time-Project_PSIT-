b74147c233ff8daf791adf61d6307e02b35c5f6d
winrhcp <winrhcp@outlook.com>
2015-12-14 00:32:54 +0700

    add values

--- Project.v2.py
+++ Project.v2.py
@@ -2,6 +2,7 @@
 import csv
 import plotly.plotly as py
 from plotly.tools import FigureFactory as FF
+import plotly.graph_objs as go
 from plotly.graph_objs import *
 from datetime import datetime
 py.sign_in('armhansa', 'um3r605dt8')
@@ -51,9 +52,10 @@ while day_in > 0:
                 low_data.append(float(row[4]))
                 close_data.append(float(row[5]))
                 vol_data.append(float(row[6]))
+                values_data.append(float(row[6])*float(row[5]))
                 dates.append(datetime(year = int(row[1][0:4]), month = int(row[1][4:6]), day = int(row[1][6:])))
         if bug == 1:
-            it_bug ##make error
+            it_bug ##if error
         day_in -= 1
     except:
         pass
@@ -100,3 +102,11 @@ fig['data'].extend([add_sma])
 fig['data'].extend([add_ema15])
 fig['data'].extend([add_ema50])
 plot_url = py.plot(fig, filename='finance/simple-candlestick', validate=False)
+
+data = [
+    go.Scatter(
+        x=dates,
+        y=values_data
+    )
+]
+plot_url = py.plot(data, filename='python-datetime')
