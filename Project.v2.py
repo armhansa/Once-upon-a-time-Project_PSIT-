e8f52c28b483c7642d6f7f3a9418d627c2215273
armhansa <s8070159@kmitl.ac.th>
2015-12-13 14:47:35 +0700

    Edit SMA Future price in v2

--- Project.v2.py
+++ Project.v2.py
@@ -1,51 +1,66 @@
 import csv
 import plotly.plotly as py
 from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
 from datetime import datetime
-py.sign_in('5z3nnr8v', 'fqyao8qk62')
-yy, mm, dd = 2015, 12, 1
-name_hoon = input('Stock Name : ').upper()
-day_in = int(input('Time : '))
-future = int(input())
-data_open, data_close, data_high, data_low, data_vol, data_values, date = [], [], [], [], [], [], []
+py.sign_in('armhansa', 'um3r605dt8')
+
+year, month, day = 2015, 12, 11
+name_stock = input('Stock Name : ').upper()
+day_in = int(input('Rate Time : '))
+future = int(input('Future : '))
+total = day_in
+open_data, close_data, high_data, low_data, vol_data, dates = [], [], [], [], [], []
 while day_in > 0:
-    step_day = 'set-history_EOD_'+'%d-%02d-%02d.csv' % (yy, mm, dd)
+    step_day = 'set-history_EOD_'+'%d-%02d-%02d.csv' % (year, month, day)
     try:
-        file = open(step_day, 'r')
-        temporary = csv.reader(file)
+        temporary = csv.reader(open(step_day, 'r'))
         for row in temporary:
-            if row[0] == name_hoon:
-                data_open.append(float(row[2]))
-                data_high.append(float(row[3]))
-                data_low.append(float(row[4]))
-                data_close.append(float(row[5]))
-                data_vol.append(float(row[6]))
-                data_values.append(float(row[6])*float(row[5]))
-                date.append(datetime(year = int(row[1][0:4]), month = int(row[1][4:6]), day = int(row[1][6:])))
+            if row[0] == name_stock:
+                open_data.append(float(row[2]))
+                high_data.append(float(row[3]))
+                low_data.append(float(row[4]))
+                close_data.append(float(row[5]))
+                vol_data.append(float(row[6]))
+                dates.append(datetime(year = int(row[1][0:4]), month = int(row[1][4:6]), day = int(row[1][6:])))
         day_in -= 1
     except:
         pass
-    data_sma, date_sma = [], []
-    for i in range(day_in):
-        if i >= future-1:
-            sma = 0
-            for j in range(future):
-                sma += data_close[::-1][i-(future-1)-j]
-            data_sma.append(sma/future)
-            date_sma.append(datetime(year = int(date[i+1][0:4]), month = int(date[i+1][4:6]), day = int(date[i+1][6:])))
-    dd -= 1
-    if dd == 0:
-        mm -= 1
-        dd = 31
-    if mm == 0:
-        yy -= 1
-        mm = 12
+
+    day -= 1
+    if day == 0:
+        month -= 1
+        day = 31
+    if month == 0:
+        year -= 1
+        month = 12
+
+open_data, close_data, high_data, low_data, vol_data, dates = open_data[::-1], close_data[::-1], high_data[::-1], low_data[::-1], vol_data[::-1], dates[::-1]
+
+data_sma, date_sma = [], []
+for i in range(total):
+    if i >= future-1:
+        sma = 0
+        for j in range(future):
+            sma += close_data[i-j]
+        data_sma.append(sma/future)
+        if i != total-1:
+            date_sma.append(dates[i+1])
+        else:
+            date_sma.append(datetime(year = 2015, month = 12, day = 12))
+
 add_line = Scatter(
     x=date_sma, 
     y=data_sma, 
-    name= 'Open Vals', 
+    name= 'SMA', 
     line=Line(color='black')
     )
-fig = FF.create_candlestick(data_open, data_high, data_low, data_close, dates=date)
+
+fig = FF.create_candlestick(open_data, high_data, low_data, close_data, dates=dates)
+fig['layout'].update({
+    'title': 'Stock',
+    'yaxis': {'title': 'Price'},
+    'xaxis': {'title': 'Time'}
+})
 fig['data'].extend([add_line])
 plot_url = py.plot(fig, filename='finance/simple-candlestick', validate=False)
